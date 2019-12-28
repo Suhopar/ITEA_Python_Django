5ae4d31a78c1ae132c41b736b6554cd41fb56432
Suhopar <51441367+Suhopar@users.noreply.github.com>
2019-12-28 09:49:52 +0200

    pass

--- practice_1/project/project/settings.py
+++ practice_1/project/project/settings.py
@@ -150,7 +150,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'freefreepost@gmail.com'
-EMAIL_HOST_PASSWORD = 'freeepostt22'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # celery
